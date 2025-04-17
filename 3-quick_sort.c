9c6d018b9384d477991b6047e9797239095de58c
tamerlahtm <tamerlan_htm@hotmail.com>
2025-04-17 09:35:42 +0000

    3quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,14 +27,22 @@ int lomuto_partition(int *array, int low, int high, int size)
 				print_array(array, size);
 			}
 			i++;
-		}
-	}
-	if (i != high)
-	{
-		temp = array[i];
-		array[i] = array[high];
-		array[high] = temp;
-		print_array(array, size);
-	}
-	return (i);
+
+			/**
+ * quicksort - quicksort
+ * @array: array
+ * @low: low
+ * @high: high
+ * @size: size of the array
+ * Return: void
+ */
+void quicksort(int *array, int low, int high, int size)
+{
+	int partition;
+
+	if (low >= high || low < 0)
+		return;
+	partition = lomuto_partition(array, low, high, size);
+	quicksort(array, low, partition - 1, size);
+	quicksort(array, partition + 1, high, size);
 }
